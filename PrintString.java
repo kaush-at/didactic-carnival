fa3dbe79e13d9a97d95585e28784232eeabb7391
kaush <emailtokaush@gmail.com>
2020-10-10 11:25:04 -0400

    added java escape charactor examples

---
+++ PrintString.java
@@ -0,0 +1,19 @@
+package com.challenges.common;
+/*
+ * write a program to print several strings using one single sysout  
+ * 
+ * Hello
+ * world 
+ * to 
+ * java
+ */
+public class PrintString {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello\nworld\nto\njava");
+		System.out.println("Good Morning Geeks! \n How are you all?  ");
+		System.out.println("Good Morning Geeks! \r How are you all?  "); 
+	}
+
+}
