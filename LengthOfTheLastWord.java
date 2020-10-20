ff71e651d75c475b5efac103cae26b34e84df081
kaush <emailtokaush@gmail.com>
2020-10-20 19:46:08 -0400

    adde another way to solve  LengthOfTheLastWord exercise

--- LengthOfTheLastWord.java
+++ LengthOfTheLastWord.java
@@ -5,8 +5,10 @@ public class LengthOfTheLastWord {
 	public static void main(String[] args) {
 		int count = getTheSizeOfLastWord(" hello   ty  www");
 		int count1 = getTheSizeOfLastWordByReversing(" hello   ty  www ");
+		int count2 = getSizeOfLastWord(" hello   ty  www ");
 		System.out.println("The length of the last word is : " + count);
 		System.out.println("The length of the last word is by second method: " + count1);
+		System.out.println("The length of the last word is by Third method: " + count2);
 	}
 
 	// Method 1 -> start iterating from the beginning
@@ -50,6 +52,20 @@ public class LengthOfTheLastWord {
 			}
 		}
 
+		return count;
+	}
+
+	// Method 3 -> cunting from the end
+	public static int getSizeOfLastWord(String word) {
+		int count = 0;
+		for (int i = word.length() - 1; i >= 0; i--) {
+			if (word.charAt(i) != ' ') {
+				count++;
+			}
+			if (count > 0 && word.charAt(i) == ' ') {
+				return count;
+			}
+		}
 		return count;
 	}
 
