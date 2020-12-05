2f48e5cc6879aa05f82efefef5a247a70035593e
kaush <emailtokaush@gmail.com>
2020-12-05 10:54:25 -0500

    FirstNonRepeatedChar solution added using map

--- FirstNonRepeatedChar.java
+++ FirstNonRepeatedChar.java
@@ -1,5 +1,8 @@
 package assignment3;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FirstNonRepeatedChar {
 
 	public static void main(String[] args) {
@@ -9,11 +12,19 @@ public class FirstNonRepeatedChar {
 		String str3 = "Hi"; // => 0
 
 		System.out.println(findFirstNonRepeatedChar(str));
-		System.out.println(findFirstNonRepeatedChar(str1));
-		System.out.println(findFirstNonRepeatedChar(str2));
-		System.out.println(findFirstNonRepeatedChar(str3));
+		System.out.println("x " + findFirstNonRepeatedChar2(str));
+
+//		System.out.println(findFirstNonRepeatedChar(str1));
+//		System.out.println("x " + findFirstNonRepeatedChar2(str1));
+//
+//		System.out.println(findFirstNonRepeatedChar(str2));
+//		System.out.println("x " + findFirstNonRepeatedChar2(str2));
+//
+//		System.out.println(findFirstNonRepeatedChar(str3));
+//		System.out.println("x " + findFirstNonRepeatedChar2(str3));
 	}
 
+	// method 1
 	public static char findFirstNonRepeatedChar(String str) {
 
 		for (int i = 0; i < str.length(); i++) {
@@ -26,6 +37,26 @@ public class FirstNonRepeatedChar {
 				}
 			}
 		}
+		return 0;
+
+	}
+
+	// method 2
+	public static char findFirstNonRepeatedChar2(String str) {
+
+		Map<Character, Integer> myMap = new LinkedHashMap<Character, Integer>();
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			System.out.println("string character " + ch);
+
+			if (myMap.containsKey(ch)) {
+				return ch;
+			} else {
+				myMap.put(ch, 1);
+			}
+		}
+
 		return 0;
 
 	}
