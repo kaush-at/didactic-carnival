3716e545a7adc0666332121da74f446172ae918a
kaush <emailtokaush@gmail.com>
2021-01-16 21:14:00 -0500

    added validation to the code to catch no majority element

--- FindTheMajorityElement.java
+++ FindTheMajorityElement.java
@@ -5,8 +5,19 @@ package com.kaush.udmy.dsa2020;
 public class FindTheMajorityElement {
 
 	public static void main(String[] args) {
-		int[] arr1 = {1,2,2,3,1,1,2,3,1};
-		int[] arr2 = {2,2,3,1,2,3,1};
+	
+/*
+ * 		these two input dosent work  for this algorithm because there are no majority element here
+ * 
+ * 		int[] arr1 = {1,2,2,1,3,1,1,2,3};
+ *		int[] arr2 = {3,2,2,3,1,2,3,2,1};
+ *		
+ *		we have to validate idf we cannot do above assumption
+ * 
+ */
+
+		int[] arr1 = {1,2,2,1,3,1,1,2,3};
+		int[] arr2 = {3,2,2,3,1,2,3,2,1};
 		int[] arr3 = {3,2,3};
 		int[] arr4 = {4,7,4,4,7,4,4,9,4,3};
 		
@@ -16,24 +27,41 @@ public class FindTheMajorityElement {
 		System.out.println(findMajorityElement(arr4));
 	}
 	
+	// to do this at least one majority element should present
 	public static int findMajorityElement(int[] arr) {
 		int majority = arr[0];
 		int count = 1;
 		
 		for (int i = 1; i < arr.length; i++) {
-			if(majority == arr[i]) {
+			if(arr[i] == majority) {
 				count++;
 			}else {
 				count--;
-				if(count <= 0) {
+				if(count == 0) {
 					majority = arr[i];
 					count=1;
 				}
 			}
 		}
 		
+		// if we cannot assume the majority element always there
+		   int majorityCount = 0;
+		   for (int i = 0; i < arr.length; i++) {
+			if(arr[i] == majority) {
+				majorityCount++;
+			 }
+		   }
 		
-		return majority;
+		   if(majorityCount > arr.length/2) {
+			   return majority;
+		   }else {
+			   return -1;
+		   }
+
+		
 	}
+	
+	
+
 
 }
