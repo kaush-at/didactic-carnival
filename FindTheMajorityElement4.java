881c10163689645c68e3e2e886ed59f4faf05d73
kaush <emailtokaush@gmail.com>
2021-01-17 21:12:29 -0500

    added another way to FindTheMajorityElement by sorting array

---
+++ FindTheMajorityElement4.java
@@ -0,0 +1,47 @@
+package com.kaush.udmy.dsa2020;
+
+import java.util.Arrays;
+
+// moore's voting approach (intuition method)
+public class FindTheMajorityElement4 {
+
+	public static void main(String[] args) {
+	
+
+
+		int[] arr1 = {1,2,2,1,3,1,1,2,3};
+		int[] arr2 = {3,2,2,3,1,2,3,2,1};
+		int[] arr3 = {3,2,3};
+		int[] arr4 = {4,7,4,4,7,4,4,9,4,3};
+		
+		System.out.println(findMajorityElement(arr1));
+		System.out.println(findMajorityElement(arr2));
+		System.out.println(findMajorityElement(arr3));
+		System.out.println(findMajorityElement(arr4));
+	}
+	
+	// to do this at least one majority element should present
+	public static int findMajorityElement(int[] arr) {
+		
+		Arrays.sort(arr);
+		int mid = arr.length/2;
+		
+		int count = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[mid]== arr[i]) {
+				count++;
+			}
+		}
+		
+		if(arr.length/2 < count) {
+			return arr[mid];
+		}else {
+			return -1;
+		}
+	}
+	
+	
+
+
+}
